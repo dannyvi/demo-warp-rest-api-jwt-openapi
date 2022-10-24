3ce06243215f4f3b7777b1d66bc316f92f1342e6
dannyvi <dannyvis@icloud.com>
2022-10-24 16:50:03 +0800

    basic service.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use crate::server::server;
+
+mod context;
+mod server;
+
+#[tokio::main]
+async fn main() {
+    server().await.expect("hyper error");
 }
---
+++ src/server.rs
@@ -0,0 +1,36 @@
+use crate::context::config::CONFIG;
+use std::{net::SocketAddr, convert::Infallible};
+
+use hyper::Server;
+use listenfd::ListenFd;
+use warp::{Filter };
+
+pub async fn server() -> Result<(), hyper::Error> {
+    let server_addr = CONFIG
+        .server
+        .as_str()
+        .parse::<SocketAddr>()
+        .expect("Unable to parse socket address");
+    let routes = warp::any().map(|| "Hello, World!");
+    
+    
+    let svc = warp::service(
+        routes
+    );
+
+    let make_svc = hyper::service::make_service_fn(|_: _| {
+        let svc = svc.clone();
+        async move { Ok::<_, Infallible>(svc) }
+    });
+
+    let mut listenfd = ListenFd::from_env();
+
+    let server = if let Ok(Some(l)) = listenfd.take_tcp_listener(0) {
+        Server::from_tcp(l).unwrap()
+    } else {
+        Server::bind(&server_addr)
+    };
+
+    server.serve(make_svc).await
+    
+}
